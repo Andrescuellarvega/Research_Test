e81c59657939e17b8af6efbb929ff9f876004f63
Andres Cuellar Vega <andres.cuellar01@utrgv.edu>
2020-02-02 22:06:58 -0600

    Changed transfer matrix C_hat for matrix W_hat, in terms of/ eigenvalues of T_hat

--- KP_Transfer_Matrix.py
+++ KP_Transfer_Matrix.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 # Transfer Matrix
 j = np.complex(0, 1)
+
+# Transfer Matrix
 def TransferMatrix(q, epsilon):
     ell = np.array(((1, 1), (j*q, -j*q)))
     v = np.array(((1, 0), (epsilon, 1)))
@@ -21,13 +23,24 @@ def TransferMatrix(q, epsilon):
 q = 1
 epsilon = 1
 bins = 1000
-x = np.linspace(1/20, 20, bins)
+limit = 20
+x = np.linspace(1/limit, limit, bins)
 n = np.ceil(x)
 
-v1_normed, v2_normed = TransferMatrix(q, epsilon)
-#v1_normed = np.array((v1_normed[1], v1_normed[0]))
+
+#v1_normed, v2_normed = TransferMatrix(q, epsilon)
+#v1_normed = np.array((v1_normed[1], v1_normed[0]))  #flip vector components
+
+
 qq = np.cos(q) + (epsilon/(2*q))*np.sin(q)
 kk = np.arccos(qq)
+ww = -np.cos(q) + (2*q/epsilon)* np.sin(q)
+vv1 = np.array((np.exp(j*q)*(ww + (2*q/epsilon)*np.sqrt(1-qq**2)), 1))
+vv2 = np.array((np.exp(j*q)*(ww - (2*q/epsilon)*np.sqrt(1-qq**2)), 1))
+
+v1_normed = vv1/np.linalg.norm(vv1)
+v2_normed = vv2/np.linalg.norm(vv2)
+
 
 exp1, exp2 = (np.zeros(bins, dtype=complex), np.zeros(bins, dtype=complex))
 if np.abs(qq) < 1:
@@ -46,13 +59,10 @@ for bin in range(0, bins):
                        + v2_normed[1] * np.exp(-j*q*(x[bin]-n[bin])))/\
                         (v2_normed[0] + v2_normed[1])
 
-#phi1 = exp1 * (v1_normed[0] * np.exp(j*q*(7.9-8))
-#                       + v1_normed[1] * np.exp(-j*q*(7.9-8)))/\
-#                        (v1_normed[0] + v1_normed[1])
-
-plt.plot(x,np.real(phi1))
+plt.plot(x, np.zeros(np.shape(x)))
+plt.plot(x, np.real(phi1))
+plt.xlim([0, limit])
 plt.show()
 
 # Eigenvectors are not coming out to what they are supposed to be when implementing np.linalg.ein(t),
-# once I use the analytic form them, the program should work out. I should find out why that isn't working out though
-# I think same thing happens in the mathematica file when imputting actual values and then calculating eigenvects
+# once I use the analytic form, the calculation works out.
