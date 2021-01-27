39926fe27d6b5987438f6b846aa769de3b67fea4
Andrescuellarvega <andrew.cuellar94@gmail.com>
2021-01-26 19:18:38 -0600

    Trying to make a version of PeriodicPotential that works for a phase shift applied at every SQUID. Work in Progress

--- Bloch_modes.py
+++ Bloch_modes.py
@@ -1,14 +1,31 @@
+import numpy as np
 import j_constants
+from input_frequencies import *
+
+# Changing constants to new units
 
 ell = j_constants.L0_eff
 QQ = 100*j_constants.omega_d*(ell/j_constants.vcpw)  # Unitless drive frequency , see Units notes
 epsilon = ell/j_constants.L0_eff
 
-n_omega = 1  # Number of sidebands used fro calculation
-n_d = 10  # Number of bins dividing (0, 2*N_Omega + 1)
+# creating grid of input frequencies
+N_sidebands = 2  # Number of sidebands used fro calculation
+N_bins = 10 # Number of bins dividing (0, 2*N_Omega + 1)
+freq_range_low, freq_range_high = (0.4802, 0.5194)
+
+frequency_grid, freq_grid_size = input_frequencies(freq_range_low, freq_range_high, N_bins, N_sidebands, j_constants.omega_d)
+print(freq_grid_size)
+
+
+#for (0 < omega_k < omega_d), (omega_d < omega_k < 2 * omega_d)
+for k in range(0, freq_grid_size):
+    ww = frequency_grid[current_k, :]
+for m in range(1, 2*N_sidebands+2):
+    ww = frequency_grid[current_k, :]
+
 
 def ww(k, n):
     ww = k*QQ/n_d + (n_omega + 1 - n) * QQ
     return ww
 
-print (ww(2,3))
+
