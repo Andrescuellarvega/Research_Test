155c489f0fc75efc3a427635bcbf37a088ab0c2b
Andrescuellarvega <andrew.cuellar94@gmail.com>
2021-02-05 21:13:15 -0600

    Trying to make a version of PeriodicPotential that works for a phase shift applied at every SQUID. Work in Progress

--- Bloch_modes.py
+++ Bloch_modes.py
@@ -9,17 +9,17 @@ QQ = 100*j_constants.omega_d*(ell/j_constants.vcpw)  # Unit-less drive frequency
 epsilon = ell/j_constants.L0_eff
 
 # Creating grid of input frequencies
-N_sidebands = 1  # Number of sidebands used fro calculation
-N_bins = 10  # Number of bins dividing (0, 2*N_Omega + 1)
+N_sidebands = 2  # Number of sidebands used fro calculation
+N_bins = 100  # Number of bins dividing (0, 2*N_Omega + 1)
 freq_range_low, freq_range_high = (0, 1)  # Bounds for the input frequencies array, divided by drive frequency.
 
-w_input, input_size = input_frequencies(freq_range_low, freq_range_high, N_bins, N_sidebands, j_constants.omega_d)
+w_input, input_shape = input_frequencies(False, freq_range_low, freq_range_high, N_bins, N_sidebands, j_constants.omega_d)
 
-q_input = w_input*100*(ell/j_constants.vcpw)  # Make inpu frequencies unitless, see Units notes
+q_input = w_input*100*(ell/j_constants.vcpw)  # Make input
+
+# frequencies unitless, see Units notes
 
 kk = np.abs(q_input)
-print(kk/(QQ))
-
 
 # From Mathematica "periodic_array_082920", the description of the k-loop goes
 # 1) Find M s.t. q_input is in [M* QQ, (M+1)*QQ] ====> here M is the index s.t. kk[M,k]
@@ -27,4 +27,11 @@ print(kk/(QQ))
 # 3) For given loop value k restrict the number of side bands s.t. abs( Cos[q(k,n)] + (epsilon/(2*q(k,n)*Sin[q(k,n)])) < 1
 #   for all n (main band corresponds to n = N_sidebands + 1, here N_sidebands since indexes start with zero)/
 
-
+for sideband in range(0, N_sidebands+2):
+    print(sideband)
+    for k in range(0, input_shape[0]):
+        q = kk[k, sideband]
+        test = np.cos(q) + (epsilon/2*q)*np.sin(q)
+        #print(test)
+        if abs(test) < 1:
+            print(sideband, k,"test passed")
--- input_frequencies.py
+++ input_frequencies.py
@@ -12,19 +12,24 @@
 # frequency grid has columns for each sideband (N_sidebands to - N_sidebands) and rows for frequencies
 
 
-def input_frequencies(freq_range_low, freq_range_high, N_bins, N_sidebands, omega_d):
+def input_frequencies(endpoints,freq_range_low, freq_range_high, N_bins, N_sidebands, omega_d):
     import numpy as np
 
     low_index = int(N_bins * freq_range_low) - 1
     high_index = int(N_bins * freq_range_high) - 1
 
     freq_grid_size = high_index - low_index + 1
+    freq_grid_shape = (freq_grid_size, 2*N_sidebands + 1)
 
-    frequency_grid = np.empty((freq_grid_size, 2 * N_sidebands + 1))
+    frequency_grid = np.empty(freq_grid_shape)
     k_space = np.array(range(low_index, high_index + 1))
 
     for k in range(0, freq_grid_size):
-        for m in range(1, 2 * N_sidebands + 2):
+        for m in range(1, 2*N_sidebands + 2):
             frequency_grid[k, m-1] = (k_space[k]+1) * (omega_d/N_bins) + (N_sidebands + 1 - m) * omega_d
 
-    return frequency_grid, freq_grid_size
+    if not endpoints:
+        frequency_grid = frequency_grid[1:-1, :]
+        freq_grid_shape = np.shape(frequency_grid)
+
+    return frequency_grid, freq_grid_shape
