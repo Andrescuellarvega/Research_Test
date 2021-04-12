e93e9d4ae35ef1364270e8a003820e3f55386162
Andres Cuellar Vega <andres.cuellar01@utrgv.edu>
2021-04-12 13:49:33 -0500

    Reproduced Mathematica results. // (Remember to feed only allowed frequencies to BlochModes function)

--- Bloch_modes.py
+++ Bloch_modes.py
@@ -1,7 +1,7 @@
 import numpy as np
 import j_constants
 from input_frequencies import *
-import time
+import matplotlib.pyplot as plt
 
 # Changing constants to new units
 
@@ -11,7 +11,7 @@ epsilon = ell/j_constants.L0_eff
 j = np.complex(0,1)
 
 # Creating grid of input frequencies
-N_sidebands = 1  # Number of sidebands used for calculation
+N_sidebands = 1 # Number of sidebands used for calculation
 N_bins = 10  # Number of bins dividing (0, 2*N_Omega + 1)
 freq_range_low, freq_range_high = (0, 1)  # Bounds for the input frequencies array, divided by drive frequency.
 
@@ -74,44 +74,27 @@ for k in range(0, input_shape[0]):
 
 
 
-#print(allowed_bands)
-
-#  We now move to calculating the bloch modes
-
-#  Shorthand notations
-
+# BLOCH MODES: Calculates C_k and D_k coefficient for input frequency q = kk[bin,band]
 def bloch_modes(q,epsilon):
 
     qqq = np.cos(q) + (epsilon / (2*q)) * np.sin(q)
-
-    #print("qqq ", qqq)
     www = -np.cos(q) + ((2*q) / epsilon) * np.sin(q)
-    #print("www ", www)
 
     vv1 = np.array([np.exp(j * q) * (www + ((2 * q)/epsilon)*np.sqrt(1-qqq**2)), 1])
-    vv2 = np.array([np.exp(j * q) * (www - ((2 * q)/epsilon)*np.sqrt(1-qqq**2)), 1])
+    #vv2 = np.array([np.exp(j * q) * (www - ((2 * q)/epsilon)*np.sqrt(1-qqq**2)), 1])
 
     v1norm = vv1/np.linalg.norm(vv1)
-    #print("v1norm ", v1norm)
     #v2norm = np.exp(-j * q) * (vv2/np.linalg.norm(vv2))
 
     kkk = np.arccos(qqq)
     x = 0
     u1raw = ((v1norm[0] * np.exp(j*(q-kkk)*(x-1))) + (v1norm[1] * np.exp(-j*(q+kkk)*(x-1))))/(v1norm[0] + v1norm[1])
-    #print("u1raw", u1raw)
-
     norm1 = (1/np.abs(v1norm[0] + v1norm[1])) * np.sqrt(
                     np.abs(v1norm[0])**2 + np.abs(v1norm[1])**2 + \
                     (v1norm[0]*np.conj(v1norm[1])*(1/(2*j*q))*(1 - np.exp(-j*2*q))) + \
                      (v1norm[1]*np.conj(v1norm[0])*(1/(-2*j*q))*(1 - np.exp(j*2*q))))
-
     u1 = u1raw/norm1
-    #print("u1", u1)
-    #print("u1 ", u1)
-
-    ##up to u1 the results are the same as in Mathematica file, u1prime is still not the same, need to check for inconsistencies.
-    u1prime = ((v1norm[0]*np.exp(j*(q-kkk))*(j*(q-kkk))) + (v1norm[1]*np.exp(-j*(q+kkk))*((-j)*(q+kkk))))/(np.abs(v1norm[0]+v1norm[1])*norm1)
-    print("u1prime ", u1prime)
+    u1prime = (1/(v1norm[0] + v1norm[1]))*(v1norm[0]*np.exp(-j*(q-kkk))*(j*(q-kkk)) + v1norm[1]*np.exp(j*(q+kkk))*((-j)*(q+kkk)))/norm1
     C_k = u1
     D_k = u1prime
 
@@ -120,9 +103,7 @@ def bloch_modes(q,epsilon):
 print(kk[5,:], allowed_bands[0,:])
 #available = q_input[111, 3] * allowed_bands[111, 3]
 
-for band in range(0,3):
+for band in range(0, 2*N_sidebands + 1):
+    C_k, D_k = bloch_modes(kk[4,band], epsilon)
+    print("C_k ", C_k, " D_k ", D_k)
 
-    C_k, D_k = bloch_modes(kk[5,band], epsilon)
-    #print("C_k ", C_k, " D_k ", D_k)
-
-#print(bloch_test)
